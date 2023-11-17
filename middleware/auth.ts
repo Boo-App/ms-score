52017b7f650ae0afb9211ef214f7d11c8b3acfc6
pierre370 <pierre.coeffeteau@hotmail.com>
2023-11-17 14:59:09 +0100

    feat/middleware&dao

---
+++ middleware/auth.ts
@@ -0,0 +1,21 @@
+//import jwt from "jsonwebtoken";
+const jwt = require("jsonwebtoken");
+
+export default function auth(req: any, res: any, next: any) {
+
+    const token = req.headers.authorization.split(" ")[1];
+    try {
+        const decryptToken = jwt.verify(token, "shhhhh");
+        if(!req.headers.email){
+            res.status(400).send({error:"Il manque un paramètre dans le headers"})
+        }
+
+        if (decryptToken.email === req.headers.email) {
+            next();
+        } else {
+            res.status(400).send({error:"Le token n'est pas valide"})
+        }
+    } catch (e) {
+        return e;
+    }
+}
