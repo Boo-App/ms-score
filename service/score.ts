d877325d2cbbbfe9822103d13ae31050576002bb
pierre370 <pierre.coeffeteau@hotmail.com>
2023-11-17 15:24:35 +0100

    feat/db&model

---
+++ service/score.ts
@@ -0,0 +1,15 @@
+import {ScoreDAO} from "../dao/score"
+import Score, {DataScore} from "../interface/score";
+
+export class ScoreService {
+
+    private scoreDAO = new ScoreDAO()
+
+    async addScore(score: DataScore, id: String) {
+        return this.scoreDAO.addScore(score, id)
+    }
+
+    async getScore(id: String) {
+        return this.scoreDAO.getScore(id)
+    }
+}
